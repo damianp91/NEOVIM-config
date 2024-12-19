7f79cfc7c17c7a1d1d9800e94bb0544b3697a4b7
Damian <d4m14n36@gmail.com>
2024-12-19 17:06:37 -0300

    Configuracion de treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,46 @@
+
+-- Parser for each lenguage of the list ensure:installed 
+return {
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  build = ":TSUpdate",
+  event = "VeryLazy",
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "luadoc",
+      "python",
+      "javascript",
+      "html",
+      "css",
+      "java",
+      "c",
+      "vimdoc",
+      "sql",
+    },
+    highlight = {
+      enable = true,
+    },
+    indent = {
+      enable = true,
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@conditional.outer",
+          ["ic"] = "@conditional.inner",
+          ["al"] = "@loop.outer",
+          ["il"] = "@loop.inner",
+        },
+      },
+    },
+  },
+}
+
