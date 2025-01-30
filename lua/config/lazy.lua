2c808efaccca824cea6dc6a12f0b4c08d8594e68
Damian <d4m14n36@gmail.com>
2025-01-30 01:48:51 -0300

    finish config nvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,6 +10,15 @@ if not vim.loop.fs_stat(lazypath) then
       "--branch=stable",
       lazypath,
   })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
