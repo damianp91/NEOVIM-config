8c6c4b99c069454b7d082e1adc8f4f83256b3fa5
DamianPosada <78286119+damianp91@users.noreply.github.com>
2025-02-06 14:13:25 -0300

    Delete lua directory

--- lua/config/init.lua
+++
@@ -1,5 +0,0 @@
-
--- Requirements for start
-require "config.settings"
-require "config.lazy"
-
--- lua/plugins/luasnip.lua
+++
@@ -1,9 +0,0 @@
-
--- All configuration in config/plugins/luasnip
-return {
-  "L3MON4D3/LuaSnip",
-  version = "v2.*",
-  main = "config.plugins.luasnip",
-  config = true,
-}
-
