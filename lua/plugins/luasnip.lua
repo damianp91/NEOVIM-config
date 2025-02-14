09759dc88a794f314ff312cbfc6c1e07e8f14bf5
Damian <d4m14n36@gmail.com>
2025-02-14 02:13:11 -0300

    Changes in luaSnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -2,6 +2,7 @@
 -- All configuration in config/plugins/luasnip
 return {
   "L3MON4D3/LuaSnip",
+  dependencies = { "rafamadriz/friendly-snippets" },
   version = "v2.*",
   main = "config.plugins.luasnip",
   config = true,
