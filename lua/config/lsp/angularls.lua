2b06fb1fd00ff49045373f8255ad57d1c6b5a255
Damian <d4m14n36@gmail.com>
2026-03-16 03:15:38 -0300

    fix: correction angular

--- lua/config/lsp/angularls.lua
+++ lua/config/lsp/angularls.lua
@@ -1,25 +1,24 @@
 local M = {}
 
 function M.setup(capabilities, on_attach)
+  local mason_path = vim.fn.stdpath("data") .. "/mason/packages"
+
+  local ts_server_path = mason_path .. "/typescript-language-server/node_modules/typescript/lib"
+  local ng_server_path = mason_path .. "/angular-language-server/node_modules/@angular/language-server"
+
   vim.lsp.config("angularls", {
     cmd = {
       "ngserver",
       "--stdio",
-      "--tsProbeLocations",
-      vim.fn.stdpath("data") ..
-        "/mason/packages/typescript-language-server/node_modules",
-      "--ngProbeLocations",
-      vim.fn.stdpath("data") ..
-        "/mason/packages/angular-language-server/node_modules",
+      "--tsProbeLocations", ts_server_path,
+      "--ngProbeLocations", ng_server_path,
     },
-
     filetypes = {
       "typescript",
       "html",
       "typescriptreact",
       "typescript.tsx",
     },
-
     root_markers = {
       "angular.json",
       "project.json",
@@ -27,7 +26,6 @@ function M.setup(capabilities, on_attach)
       "tsconfig.json",
       ".git",
     },
-
     capabilities = capabilities,
     on_attach = on_attach,
   })
