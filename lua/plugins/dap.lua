49abaf90541e0076c241605f8a1b1d3521c00b81
Damian <d4m14n36@gmail.com>
2026-04-26 22:43:00 -0300

    fix: correction config dap.lua

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -5,14 +5,23 @@ return {
     -- ui plugins to make debugging simplier
     "rcarriga/nvim-dap-ui",
     "nvim-neotest/nvim-nio",
+    "theHamsta/nvim-dap-virtual-text"
   },
   cmd = { "DapContinue", "DapToggleBreakpoint" },
   config = function()
     -- gain access to the dap plugin and its functions
     local dap = require("dap")
     local dapui = require("dapui")
+    local dapvirt = require("nvim-dap-virtual-text")
 
     dapui.setup()
+
+    dapvirt.setup({
+      enable = true,
+      virtual_text = {
+        prefix = " ●",
+      },
+    })
 
     -- gain access to the dap ui plugin and its functions
     dap.listeners.before.attach.dapui_config = function()
@@ -34,10 +43,22 @@ return {
     )
     -- set a vim motion for <Space> + d + s to start the debugger and launch
     -- the debugging ui
-    vim.keymap.set("n", "<leader>ds", dap.continue, { desc = "Debug: Start" })
+    vim.keymap.set("n", "<leader>ds", dap.continue, { desc = "Debug: Start/Continue" })
     -- set a vim motion to close the debugging ui
-    vim.keymap.set("n", "<leader>dc", dapui.close, {desc = "Debug: Close"})
+    vim.keymap.set("n", "<leader>dc", function ()
+      dapui.close()
+    end, {desc = "Debug: Close"})
 
-    vim.keymap.set('n', '<Leader>dr', dap.repl.open, { desc = "Debug: Open REPL" })
+    vim.keymap.set('n', '<Leader>dr', function ()
+      dap.repl.open()
+    end, { desc = "Debug: Open REPL" })
+
+    -- Step controls
+    vim.keymap.set("n", "<leader>dk", dap.up, { desc = "Debug: Step Up"})
+    vim.keymap.set("n", "<leader>dj", dap.down, {desc = "Debug: Step Down"})
+    vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "Debug: Step Into" })
+    vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "Debug: Step Over" })
+    vim.keymap.set("n", "<leader>du", dap.step_out, { desc = "Debug: Step Out" })
+    vim.keymap.set("n", "<leader>dq", dap.terminate, { desc = "Debug: Quit" })
   end
 }
