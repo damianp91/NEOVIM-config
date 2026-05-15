baa5da5012ddc9bc012fb616434c386cdf481873
Damian <d4m14n36@gmail.com>
2026-05-15 01:20:05 -0300

    fix: snacks/image render

--- lua/core/config_snacks/image.lua
+++ lua/core/config_snacks/image.lua
@@ -1,29 +1,37 @@
 return {
   enabled = true,
   doc = {
-    float = true,   -- show image on cursor hover
+    float = false,   -- show image on cursor hover
     inline = false, -- show image inline
     max_width = 50,
     max_height = 30,
     stay_on_hover = true,
+    timeout = 500,
     wo = {
       wrap = false,
+      cursorline = true
     },
   },
   convert = {
     notify = true,
-    command = "magick"
+    command = "magick",
+    max_size = {width = 1920, height = 1080},
+    timeout = 3000
   },
+  formats = {"png", "jpg", "jpeg", "gif", "webp"},
   img_dirs = {
-    "img",
-    "images",
-    "assets",
-    "static",
-    "public",
-    "media",
-    "attachments",
-    "Archives/All-Vault-Images/",
-    "~/Library",
-    "~/Descargas/"
+    -- "img",
+    -- "images",
+    -- "assets",
+    -- "static",
+    -- "public",
+    -- "media",
+    -- "attachments",
+    -- "Archives/All-Vault-Images/",
+    "images"
   },
+  cache = vim.fn.stdpath('cache') .. "/sanaks-image",
+  math = {
+    enabled = false
+  }
 }
